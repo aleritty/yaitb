04059303f25f96ee4caf33a6049374ad7c7969c1
AleRitty <aleritty@gmail.com>
2015-09-30 22:19:05 +0200

    reordered

--- EDITME/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$BOTTOKEN = 'TOKEN';
-$BOT_NAME = 'YetAnotherIdioticBot';
-$PROJECTNAME = 'GAE-PROJECTNAME';
-$BASEURL = 'https://'.$PROJECTNAME.'.appspot.com/'; //change this if not running on Google Appengine
-$ADMINID = ['int']; //ID of administrators users (not yet used)
-
-?>
--- me.php
+++ me.php
@@ -1,6 +1,6 @@
 <?php
 require_once('EDITME/config.php');
-require_once('EDITME/boot.php');
+require_once('boot.php');
 use Telegram\Bot\Api;
 $telegram = new Api($BOTTOKEN);
 $response = $telegram->getMe($BASEURL."webhook");
